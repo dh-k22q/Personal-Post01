3addabb1b88fcf5fccb5946ced511a4557a5dea9
dh-k22q <97339777+dh-k22q@users.noreply.github.com>
2022-08-18 09:49:35 +0900

    Add files via upload

---
+++ again03/src/test/java/com/sparta/again03/Again03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.again03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Again03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
